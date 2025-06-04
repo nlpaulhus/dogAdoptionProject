0b462508dadc6a4d63b0fdd0557d9fecaa884c53
Nicole Paulhus <nlpaulhus@gmail.com>
2025-06-04 14:23:57 -0700

    Added login and signup forms with bootstrap

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const app = express();
-const ejs = require('ejs');
+const ejs = require("ejs");
 
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 
 app.get("/signup", (req, res) => {
-  res.sendFile("./views/signup.html", { root: __dirname });
+  res.render("signup");
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile("./views/login.html", { root: __dirname });
+  res.render("login");
 });
 
 app.get("/registered", (req, res) => {
-  res.sendFile("./views/registered.html", { root: __dirname });
+  res.render("registered");
 });
 
 app.get("/adopted", (req, res) => {
-  res.sendFile("./views/adopted.html", { root: __dirname });
+  res.render("adopted");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.use((req, res) => {
-  res.sendFile("./views/404.html", { root: __dirname });
+  res.render("404");
 });
 
 app.listen(3000, () => {
